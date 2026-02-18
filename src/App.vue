ad457c2a51d1b0a84c0df8136e78558dfd74236d
logonaut.eth <19627089+logonaut@users.noreply.github.com>
2026-02-18 14:59:27 -0600

    change app view layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view />
+  <div class="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 font-sans">
+    <router-view />
+  </div>
 </template>
 
 <script setup>
